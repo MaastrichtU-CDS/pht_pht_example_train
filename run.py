be3f1844e6a0e11b518f9375a18437fa03054b82
AnanyaCN <ananya.choudhury@maastro.nl>
2020-07-27 17:00:39 +0200

    Modified run.py

--- run.py
+++ run.py
@@ -2,34 +2,26 @@ import os
 import pandas as pd
 import json
 
+#This part consists reading database url from env variable and setting the data query
+
+# read database uri
 database_uri = os.getenv('DATABASE_URI','no_database_uri')
 
-test = pd.read_csv(database_uri)
-mean = test['Age'].mean()
+# read/query data from database_uri
+data = pd.read_csv(database_uri)
 
-result = {'Average age':mean}
+
+## paste your algorithm here . Example to calculate mean age from the data
+count = len(data)
+mean = data['Age'].mean()
+
+#json code your output
+result = {
+    'Number of Rows': count,
+    'Average age':mean
+}
 
 # Write output to file
 with open('output.txt', 'w') as f:
     f.write(json.dumps(result))
 
-
-
-
-
-#host = os.getenv('HOST','no host')
-#api_path = os.getenv('API_PATH','no api path')
-#port = os.getenv('PORT','no port')
-#data = pd.read_csv(database_uri)
-#print(data)
-
-# results = {
-#         'database': data,
-#
-#     }
-
-
-#print(results)
-
-# with open('output.txt', 'w') as f:
-#     f.write(json.dumps(results))
